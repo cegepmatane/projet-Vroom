8afa046922876e1f7afd5674b8a65f0308729cdf
Mdaiglevigneault <44245132+Mdaiglevigneault@users.noreply.github.com>
2021-01-25 13:39:34 -0500

    fonction de generation aleatoire et options RoadManager

--- Vrooom/Assets/Scripts/RoadManager.cs
+++ Vrooom/Assets/Scripts/RoadManager.cs
@@ -9,31 +9,57 @@ public class RoadManager : MonoBehaviour
         semi,
         full
     };
+
     [Header("Options")]
+    [Tooltip("Nombre d'objets de type Road qui compose une map. une valeur négative entraine une génération infini")]
+    [SerializeField]
+    int nombreDeMap = 1;
     [SerializeField]
     GenerationProcedural generationProcedural = GenerationProcedural.semi;
 
     [Header("Options Semi Procédural")]
     [SerializeField]
-    List<Road> roadPrefabsSemiProc;
+    int toursParMap = 1;
+
+    [SerializeField]
+    List<GameObject> roadPrefabsSemiProc;
 
 
     [Header("Options Full Procédural")]
     [SerializeField]
-    List<Road> roadPrefabsFullProc;
+    List<GameObject> roadPrefabsFullProc;
 
     [SerializeField]
-    List<GameObject> roadprefabs = new List<GameObject>();
+    List<GameObject> map = new List<GameObject>();
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        generateNext();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void generateNext() {
+        if (nombreDeMap > 0 && map.Count >= nombreDeMap) { return; }
+
+        //get un segment (Road) random
+        int indexNext = (int)Random.Range(0, roadPrefabsSemiProc.Count);
+
+        GameObject nextPrefab = null;
+        switch (generationProcedural) {
+            case GenerationProcedural.semi:
+                nextPrefab = roadPrefabsSemiProc[indexNext];
+                break;
+            case GenerationProcedural.full:
+                nextPrefab = roadPrefabsFullProc[indexNext];
+                break;
+            default:
+                break;
+        }
+
+        map.Add(Instantiate(nextPrefab));
+
+        if (map.Count == nombreDeMap) {
+            Debug.Log("Dernière Road!");
+            //map[map.Count - 1];
+            //TODO map.setFinishLine();
+        }
     }
 }
