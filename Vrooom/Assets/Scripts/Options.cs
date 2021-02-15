ab35292e72730ada277635e06c6141a7ee9a0b26
GVHLNC <hlnc.app@gmail.com>
2021-02-15 16:02:14 -0500

    Modification interface
    Correction toggle essaie

---
+++ Vrooom/Assets/Scripts/Options.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static RoadManager;
+
+public class Options : MonoBehaviour
+{
+
+    //private Options options = null;
+    public static GenerationProcedural modeProcedural;
+    public static int nbrSegment;
+    public static int nbrCarte;
+    public static int nbrTours;
+
+    
+
+    //private Options getInstance() { return options = options == null ? new Options() : options; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //GenerationProcedural.semi
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
