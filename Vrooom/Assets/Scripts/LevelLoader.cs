27587616844340107a110e2b88e2058cfd954302
GVHLNC <hlnc.app@gmail.com>
2021-03-19 00:53:41 -0400

    Correction Bug affichage background + LevelLoader
    Suppression des transitionsTime, ajout du fond dans le menu principale

--- Vrooom/Assets/Scripts/LevelLoader.cs
+++ Vrooom/Assets/Scripts/LevelLoader.cs
@@ -8,29 +8,24 @@ public class LevelLoader : MonoBehaviour
     private int IndexSceneGameplay = 1;
     private int IndexSceneFin = 2;
 
-    public Animator transition;
-    public float transitionTime = 1f;
-
     void Update()
     { }
 
     public void LoadMenu()
     {
-        StartCoroutine(LoadLevel(IndexSceneMenu));
+        LoadLevel(IndexSceneMenu);
     }
     public void LoadGameplay()
     {
-        StartCoroutine(LoadLevel(IndexSceneGameplay));
+        LoadLevel(IndexSceneGameplay);
     }
     public void LoadFin()
     {
-        StartCoroutine(LoadLevel(IndexSceneFin));
+        LoadLevel(IndexSceneFin);
     }
 
-    IEnumerator LoadLevel(int levelIndex)
+    public void LoadLevel(int levelIndex)
     {
-        //transition.SetTrigger("Start");
-        yield return new WaitForSeconds(transitionTime);
         SceneManager.LoadScene(levelIndex);
     }
 
